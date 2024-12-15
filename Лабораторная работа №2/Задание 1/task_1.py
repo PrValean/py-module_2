4fb8e27a4bae8f25843db0fb3ea7f5de12c20dd7
PrValean <145262194+PrValean@users.noreply.github.com>
2024-12-15 22:14:07 +0300

    Update task_1.py

--- Лабораторная работа №2/Задание 1/task_1.py
+++ Лабораторная работа №2/Задание 1/task_1.py
@@ -2,12 +2,12 @@ BOOKS_DATABASE = [
     {
         "id": 1,
         "name": "test_name_1",
-        "pages": 200,
+        "pages": 200
     },
     {
         "id": 2,
         "name": "test_name_2",
-        "pages": 400,
+        "pages": 400
     }
 ]
 
